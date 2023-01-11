8ac09ab0e6089179b8ebd06179ba3a3fe5bcdfcb
JulioCesarCode <julio111499@gmail.com>
2023-01-11 12:36:33 -0500

    end course

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
-from fastapi import FastAPI, Body, Path, Query
+#from fastapi import FastAPI, Body, Path, Query
+#from fastapi.responses import HTMLResponse, JSONResponse
+#from pydantic import BaseModel, Field
+from fastapi import FastAPI, Body, Path, Query, Request, Depends, HTTPException
+
 from fastapi.responses import HTMLResponse, JSONResponse
+#from fastapi.responses import HTMLException
 from pydantic import BaseModel, Field
 from typing import Optional, List
+from jwt_manager import create_token, validate_token
+from fastapi.security import HTTPBearer
+
+
+#from typing import Optional, List
 
 app = FastAPI()
 app.title = "Mi aplicación con  FastAPI"
 app.version = "0.0.1"
+
+class JWTBearer (HTTPBearer):
+    async def __call__(self, request: Request):
+        auth =await super().__call__(request)
+        data= validate_token(auth.credentials)
+        if data[ 'email']!='admin@gmail.com':
+            raise HTTPException(status_code=403, detail='invalid credentials')
+
+class User(BaseModel):
+    email:str
+    password:str
+
 
 class Movie(BaseModel):
     id: Optional[int] = None
@@ -50,28 +72,34 @@ movies = [
 def message():
     return HTMLResponse('<h1>Hello world</h1>')
 
-@app.get('/movies', tags=['movies'], response_model=List[Movie])
-def get_movies() -> List[Movie]:
-    return JSONResponse(content=movies)
+@app.post('/login', tags=[ 'auth'])
+def login(user: User):
+    if user.email == 'admin@gmail.com' and user.password== 'admin':
+        token:str = create_token(user.dict())
+        return JSONResponse(status_code=200, content=token)
 
-@app.get('/movies/{id}', tags=['movies'], response_model=Movie)
+@app.get('/movies', tags=['movies'], response_model=List[Movie], status_code=200, dependencies=[Depends(JWTBearer())])
+def get_movies() -> List[Movie]:
+    return JSONResponse(status_code=200,content=movies)
+
+@app.get('/movies/{id}', tags=['movies'], response_model=Movie, status_code=200)
 def get_movie(id: int = Path(ge=1, le=2000)) -> Movie:
     for item in movies:
         if item["id"] == id:
-            return JSONResponse(content=item)
-    return JSONResponse(content=[])
+            return JSONResponse(status_code=200, content=item)
+    return JSONResponse(status_code=404, content=[])
 
 @app.get('/movies/', tags=['movies'], response_model=List[Movie])
 def get_movies_by_category(category: str = Query(min_length=5, max_length=15)) -> List[Movie]:
     data = [ item for item in movies if item['category'] == category ]
     return JSONResponse(content=data)
 
-@app.post('/movies', tags=['movies'], response_model=dict)
+@app.post('/movies', tags=['movies'], response_model=dict, status_code=201)
 def create_movie(movie: Movie) -> dict:
     movies.append(movie)
-    return JSONResponse(content={"message": "Se ha registrado la película"})
+    return JSONResponse(status_code=201, content={"message": "Se ha registrado la película"})
 
-@app.put('/movies/{id}', tags=['movies'], response_model=dict)
+@app.put('/movies/{id}', tags=['movies'], response_model=dict, status_code=200)
 def update_movie(id: int, movie: Movie)-> dict:
 	for item in movies:
 		if item["id"] == id:
@@ -80,11 +108,11 @@ def update_movie(id: int, movie: Movie)-> dict:
 			item['year'] = movie.year
 			item['rating'] = movie.rating
 			item['category'] = movie.category
-			return JSONResponse(content={"message": "Se ha modificado la película"})
+			return JSONResponse(status_code=200, content={"message": "Se ha modificado la película"})
 
-@app.delete('/movies/{id}', tags=['movies'], response_model=dict)
+@app.delete('/movies/{id}', tags=['movies'], response_model=dict, status_code=200)
 def delete_movie(id: int)-> dict:
     for item in movies:
         if item["id"] == id:
             movies.remove(item)
-            return JSONResponse(content={"message": "Se ha eliminado la película"})
+            return JSONResponse(status_code=200, content={"message": "Se ha eliminado la película"})
