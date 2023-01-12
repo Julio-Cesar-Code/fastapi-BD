c91e122be6699602571390d40f04267860850300
JulioCesarCode <julio111499@gmail.com>
2023-01-11 20:35:46 -0500

    App finished

---
+++ routers/users.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter
+
+from fastapi.responses import  JSONResponse
+from schemas.user import User
+from utils.jwt_manager import create_token
+
+
+
+users_routers=APIRouter()
+
+
+
+@users_routers.post('/login', tags=['auth'])
+def login(user: User):
+    if user.email == "admin@gmail.com" and user.password == "admin":
+        token: str = create_token(user.dict())
+        return JSONResponse(status_code=200, content=token)
